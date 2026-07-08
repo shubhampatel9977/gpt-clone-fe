d7bf560806bd9979061fcbf4513cc86534ed5c42
shubhampatel9977 <shubhampatel99260@gmail.com>
2026-07-08 23:00:57 +0530

    Add loader in google auth

--- src/features/auth/components/GoogleLoginButton.tsx
+++ src/features/auth/components/GoogleLoginButton.tsx
@@ -2,11 +2,20 @@ import { GoogleLogin } from "@react-oauth/google";
 import toast from "react-hot-toast";
 import { type GoogleLoginResponse, useGoogleLogin } from "../api";
 import { type AuthState, useAuthStore } from "../store";
+import { LoaderCircle } from "lucide-react";
 
 const GoogleLoginButton = () => {
-	const { mutate: googleLogin } = useGoogleLogin();
+	const { mutate: googleLogin, isPending } = useGoogleLogin();
 
 	const setLoginData = useAuthStore((state) => state.login);
+
+	if(isPending) {
+		return (
+			<div className="flex py-3 items-center justify-center text-lightGray">
+				<LoaderCircle className="animate-spin" />
+			</div>
+		)
+	}
 
 	return (
 		<div className="w-full">
@@ -20,36 +29,30 @@ const GoogleLoginButton = () => {
 						return;
 					}
 
-					googleLogin(
-						{
-							token,
+					googleLogin({token}, {
+						onSuccess: (res: GoogleLoginResponse) => {
+							if (!res.data) {
+								return;
+							}
+
+							const payload: Partial<AuthState> = {
+								user: {
+									userId: res.data.id,
+									userName: res.data.name,
+									userEmail: res.data.email,
+									role: res.data.role,
+								},
+								isLoggedIn: true,
+							};
+
+							setLoginData(payload);
+							toast.success("Login successful");
 						},
-						{
-							onSuccess: (res: GoogleLoginResponse) => {
-								if (!res.data) {
-									return;
-								}
 
-								const payload: Partial<AuthState> = {
-									user: {
-										userId: res.data.id,
-										userName: res.data.name,
-										userEmail: res.data.email,
-										role: res.data.role,
-									},
-									isLoggedIn: true,
-								};
-
-								setLoginData(payload);
-
-								toast.success("Login successful");
-							},
-
-							onError: (err) => {
-								toast.error(err.message || "Failed to login with Google");
-							},
+						onError: (err) => {
+							toast.error(err.message || "Failed to login with Google");
 						},
-					);
+					});
 				}}
 				onError={() => {
 					toast.error("Google login failed");
